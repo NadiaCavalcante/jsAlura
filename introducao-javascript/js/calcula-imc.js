5372f550d83d819b2473ca7ab782598225ccd034
Nadia C. da Silva <nadia.silva@ITBAM.ORG.BR>
2022-01-04 10:31:52 -0400

    teste

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,81 @@
+//QuerySelector("tag") - seleção dentro do arquivo html
+
+// var titulo = document.querySelector("h1") //identifica elemento h1 na página
+// console.log(titulo);
+// console.log(titulo.textContent) //representa o conteúdo de texto
+
+// titulo.textContent = "Salão Shalonie"
+
+// var pacientes = document.querySelector("h2")
+
+//  pacientes.textContent = "Meus pacientes"
+
+
+var titulo = document.querySelector(".titulo")
+titulo.textContent = "Aparecida Nutrição"
+
+var pacientes = document.querySelectorAll(".paciente")
+
+// for( var i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+for( var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso= paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent;
+
+    var tdaltura = paciente.querySelector(".info-altura")
+    var altura = tdaltura.textContent;
+
+    var tdImc =  paciente.querySelector(".info-imc")
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso Inválido")
+        pesoValido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if ( altura <= 0 || altura >= 3.00) {
+        console.log("Altura Inválida")
+        alturaValida = false;
+        tdImc.textContent = "Altura Inválida"
+        paciente.classList.add("paciente-invalido")
+
+    }
+
+    if (alturaValida && pesoValido){
+        var imc = calculaImc(peso, altura)
+        tdImc.textContent = imc
+
+    }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura)
+
+    return imc.toFixed(2); 
+    
+
+}
+
+//função anônima
+// titulo.addEventListener("click", function(){
+
+// }) 
+
+
+
+
+
+
+
+
+
---
+++ introducao-javascript/js/form.js
@@ -0,0 +1,85 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente")
+botaoAdicionar.addEventListener("click", function(event){
+    
+    event.preventDefault(); //previne o comportamento pradão do site - enviar o formo e retorna em branco. Usando este evente fazemos com que ao carregar o site a função abaixo seja exibida
+    
+    var form = document.querySelector("#form-adiciona")
+     
+    //obtem informações do paciente do form
+    var paciente = obtemPacienteForm(form)
+
+     //cria a tr e td do paciente
+    var pacienteTr = montaTr(paciente)
+     
+     //adicionando o paciente na tabela
+    var tabela = document.querySelector("#tabela-pacientes")
+
+    tabela.appendChild(pacienteTr)
+
+    form.reset() //apaga os dados inseridos no campo depois que vc adiciona
+})
+
+    function obtemPacienteForm(form) {
+
+        var paciente = {
+            nome: form.nome.value,
+            peso: form. peso.value,
+            altura: form.altura.value,
+            gordura: form.gordura.value,
+            imc: calculaImc(form.peso.value, form.altura.value)
+        }
+        
+        return paciente;
+
+    }
+
+    function montaTr(paciente) {
+        var pacienteTr = document.createElement("tr")
+        pacienteTr.classList.add("paciente")
+
+        // var nomeTd = montaTd(paciente.nome, "info-nome")
+        // var pesoTd = montaTd(paciente.peso, "info-peso")
+        // var alturaTd = montaTd(paciente.altura, "info-altura")
+        // var gorduraTd = montaTd(paciente.gordura, "info-gordura")
+        // var imcTd = montaTd(paciente.imc, "info-imc")
+
+        // var nomeTd = document.createElement("td")
+        // var pesoTd = document.createElement("td")
+        // var alturaTd = document.createElement("td")
+        // var gorduraTd = document.createElement("td")
+        // var imcTd = document.createElement("td")
+
+        // nomeTd.textContent = paciente.nome
+        // pesoTd.textContent = paciente.peso
+        // alturaTd.textContent = paciente.altura
+        // gorduraTd.textContent = paciente.gordura
+        // imcTd.textContent = paciente.imc
+
+        // var pesoTd = montaTd(paciente.peso, "info-peso")
+        
+
+        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome")) // coloca o Td dentro do Tr
+        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"))
+        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"))
+        pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"))
+        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"))
+
+        return pacienteTr
+    }
+
+    function montaTd(dado, classe) {
+        var td = document.createElement("td")
+        td.textContent = dado
+        td.classList.add(classe)
+
+        return td;
+    }   
+
+
+    
+
+    
+    //O comportamento padrão do botão que se encontra no formulário é limpar os dados preenchidos nos campos, recarregar a página e enviar os dados.
+    //são funções que não tem nome e só são chamadas no contexto aonde foram declaradas
+
+
