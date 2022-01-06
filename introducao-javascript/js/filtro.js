eb9957c7397d867dd3eea606701becbd5a8a5d7d
Nadia C. da Silva <nadia.silva@ITBAM.ORG.BR>
2022-01-06 13:49:12 -0400

    requisição AJAX

--- introducao-javascript/js/filtro.js
+++ introducao-javascript/js/filtro.js
@@ -1,2 +1,29 @@
+var campoFiltro = document.querySelector("#filtrar-tabela")
 
-// aula 8 ...5:36
+campoFiltro.addEventListener("input", function(){
+    // console.log(campoFiltro.value) - obtem o resultado do que vc digitar
+     console.log(this.value)
+
+    var pacientes = document.querySelectorAll(".paciente")
+
+    if( this.value.length > 0){
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i") // RegExp - recebe dois parâmetros (o que ela deve procurar e como)
+        
+            if (!expressao.test(nome)){  //invertemos a lá
+                paciente.classList.add("invisivel")
+            } else {
+                paciente.classList.remove("invisivel")
+            }
+    }
+    }else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i]
+            paciente.classList.remove("invisivel")
+        }
+    }    
+
+})
